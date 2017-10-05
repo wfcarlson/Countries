864b8460808461dd2c17465cabfc53bdb8c61a51
walter <walterc@vt.edu>
2017-10-05 14:24:41 -0400

    homepage

--- src/main/java/countries/Application.java
+++ src/main/java/countries/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Country and state API";
     }
 
     public static void main(String[] args) {
